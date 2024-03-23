db6f87653b087568458162093b2106252e9b92b2
Wanying Zhang <mimi@Wanyings-MacBook-Air.local>
2024-03-23 17:40:11 -0500

    collisions and object picking

--- object.py
+++ object.py
@@ -1,10 +1,11 @@
+from pyxel import *
+
 class Equipment:
 
     def __init__(self, name, x, y) -> None:
         self._name = name # string specify the name of the tool
         self._x = x
         self._y = y
-
 
 class Worm:
     def __init__(self, x, y) -> None:
@@ -15,3 +16,18 @@ class Worm:
     def move_change_loc(self, x_change, y_change):
         self._x += x_change
         self._y += y_change
+
+class Safe: 
+    def __init__(self, name, x, y, text) -> None:
+        self._name = name
+        self._x = x
+        self._y = y
+        self._text = text
+
+    def open_safe(self) -> bool:
+        text(self._x, self._y, "Would you like to open the safe?", 2)
+        if btnp(KEY_Y):
+            text(self._x, self._y, "correct", 8)
+            return True
+        else:
+            return False
--- program.py
+++ program.py
@@ -1,12 +1,13 @@
 from pyxel import *
 from background import *
 from player import Player
+from object import Safe
 from background import Tilemap, sprite
 init(256, 256, fps=15)
 load('astronaut.pyxres')
 
 tm = Tilemap()
-#player = Player()
+# player = Player()
 player_x = 1
 player_y = 1
 
@@ -22,15 +23,35 @@ def draw_sprite(player_x, player_y, frame):
         sprite(player_x+1, player_y, 3,0)
         sprite(player_x+1, player_y+1, 3,1)
 
+knife = Safe("knife", 25, 24, "ehewif") # 25 24 27 26 (top left, lower right)
+suit = Safe("suit", 25, 4, "awhef") # 25 4 27 5
+letter = Safe("letter", 9, 26, "awejfio") # 9 26 11 28
+key = Safe("key", 27, 26, "whaeh")
+rations = Safe("rations", 27, 5, "rations")
+backpack = Safe("backpack", 11, 28, "waejf")
+
+safes = [knife, suit, letter, key, rations, backpack]
 # code for opening safe, only uncomment after object.py is done
-# def openSafe(safe, player_x):
-#     for pos in safe_pos: # dont for each position in the safe positions
-#         if player_x == safe.x and player_y == safe.y: # check for collision
-#             set_tool(safe.object) # sets the tool that has been picked up from safe
-#             text(safe.x, safe.y, safe.text, 7) # displays text where the safe is opened
-#             return True
-#         else:
-#             return False
+def openSafe(safes, player_x, player_y):
+    
+    text(10, 10, f"ur coords: {player_x}, {player_y}", 7)
+    for safe in safes: # dont for each position in the safe positions
+        text(20, 20, f"safe coords: {safe._x}, {safe._y}", 2)
+        if player_x <= safe._x + 1 and player_x >= safe._x - 1 and player_y <= safe._y + 1 and player_y >= safe._y - 1: # check for collision
+            # set_tool(safe.text) # sets the tool that has been picked up from safe
+            # text(safe.x, safe.y, safe.text, 7) # displays text where the safe is opened
+            text(10, 10, "Would you like to open the safe? Y/N", 3)
+            if btnp(KEY_Y):
+                text(10, 20, "Not so fast... First you must solve a riddle. (Press A to continue)", 7)
+                if btnp(KEY_A):
+                    safe.open_safe()
+                else:
+                    text(10, 20, "Ok. Sad :'(.", 6)
+            else:
+                text(10, 20, "Very sad :'(", 5)
+            
+                
+    return True
 
 while True:
     move = False
@@ -38,7 +59,7 @@ while True:
     py = player_y
     #pl = (player_x//8, player_y//8)
     cls(0)
-
+    
     # player movement
     if btn(KEY_RIGHT):
         move = True
@@ -58,5 +79,6 @@ while True:
     if move:
         draw_sprite(player_x,player_y, 2)
     draw_sprite(player_x,player_y, 1)
+    openSafe(safes, player_x, player_y)
     flip()
 
