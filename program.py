146d05b298b10e6736abdd5e6ef9c14a11df0b17
K0an <k0an@K0ans-MacBook-Pro.local>
2024-03-23 13:56:47 -0500

    merge

--- program.py
+++ program.py
@@ -1,7 +1,6 @@
 from pyxel import *
 from background import *
 from player import Player
-<<<<<<< HEAD
 from background import Tilemap, sprite
 init(128, 128, fps=10)
 load('astronaut.pyxres')
@@ -23,7 +22,6 @@ def draw_sprite(player_x, player_y, frame):
         sprite(player_x+1, player_y, 3,0)
         sprite(player_x+1, player_y+1, 3,1)
 
-=======
 from background import Tilemap
 init(64, 64, fps=3)
 
@@ -34,21 +32,18 @@ tm = Tilemap()
 player = Player()
 player_x = 1
 player_y = 1
-
->>>>>>> 864b4ff33a703542bd7877ebb9924b56f7a09775
 while True:
     move = False
     px = player_x
     py = player_y
     pl = (player_x//8, player_y//8)
     cls(0)
+
+    # player movement
     if btn(KEY_RIGHT):
-<<<<<<< HEAD
         player_x += 1
         move = True
-=======
         player_x = tm.scroll(player_x)
->>>>>>> 864b4ff33a703542bd7877ebb9924b56f7a09775
     elif btn(KEY_LEFT):
         player_x -= 1
         move = True
@@ -58,26 +53,19 @@ while True:
     elif btn(KEY_DOWN):
         player_y += 1
         move = True
+    
+
     if tm.get(0, player_x%8, player_y%8) == (0, 1) and (player_x//8, player_y//8) == pl:
         player_x = px
         player_y = py 
     elif tm.get(0, player_x%8, player_y%8) == (1, 1):
         player_x = 1
         player_y = 1
-<<<<<<< HEAD
     tm.x = player_x//8
     tm.y = player_y//8
     if move == True:
         draw_sprite(player_x,player_y, 2)
     draw_sprite(player_x,player_y, 1)
-=======
-    tm.player_x = player_x//8
-    tm.player_y = player_y//8
-    sprite(player_x % 4, player_y % 4, 0, 0)
-    sprite(player_x % 4, player_y % 4 + 1, 0, 1)
-    sprite(player_x % 4 + 1, player_y % 4, 1, 0)
-    sprite(player_x % 4 + 1, player_y % 4 + 1, 1, 1)
->>>>>>> 864b4ff33a703542bd7877ebb9924b56f7a09775
     tm.draw(0)
     flip()
 
