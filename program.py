18e2e16465f7c598125cf1b3b9a0c3657bc141c0
K0an <k0an@K0ans-MacBook-Pro.local>
2024-03-23 13:03:19 -0500

    imports

--- program.py
+++ program.py
@@ -1,32 +1,35 @@
 from pyxel import *
 from background import *
+from player import Player
+from background import Tilemap
 init(64, 64, fps=3)
-load('my_resource.pyxres')
+load('astronaut.pyxres')
 
 tm = Tilemap()
-x = 1
-y = 1
+player = Player()
+player_x = 1
+player_y = 1
 while True:
-    px = x
-    py = y
-    pl = (x//8, y//8)
+    px = player_x
+    py = player_y
+    pl = (player_x//8, player_y//8)
     cls(0)
     if btn(KEY_RIGHT):
-        x += 1
+        player_x += 1
     elif btn(KEY_LEFT):
-        x -= 1
+        player_x -= 1
     elif btn(KEY_UP):
-        y -= 1
+        player_y -= 1
     elif btn(KEY_DOWN):
-        y += 1
-    if tm.get(0, x%8, y%8) == (0, 1) and (x//8, y//8) == pl:
-        x = px 
-        y = py 
-    elif tm.get(0, x%8, y%8) == (1, 1):
-        x = 1
-        y = 1
-    tm.x = x//8
-    tm.y = y//8
-    sprite(x%8, y%8, 1, 0)
+        player_y += 1
+    if tm.get(0, player_x%8, player_y%8) == (0, 1) and (player_x//8, player_y//8) == pl:
+        player_x = px
+        player_y = py 
+    elif tm.get(0, player_x%8, player_y%8) == (1, 1):
+        player_x = 1
+        player_y = 1
+    tm.player_x = player_x//8
+    tm.player_y = player_y//8
+    sprite(player_x%4, player_y%4, 1, 0)
     tm.draw(0)
     flip()
