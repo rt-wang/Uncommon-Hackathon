68a7934a39b19278e761fb8ec4ea93bcccb40edf
yichending01 <yichending01@gmail.com>
2024-03-23 21:55:09 -0500

    player animation

--- program.py
+++ program.py
@@ -90,6 +90,7 @@ for i in range(100):
     worm_lst.append(worm)
 
 worm_frame = 0
+player_frame = 0
 
 
 while True:
@@ -180,14 +181,14 @@ while True:
 
     if "knife" in player.tool_names():
         if not attack:
-            if move:
+            if move and player_frame%5 < 3:
                 draw_sprite(player_x,player_y,4)
             else:
                 draw_sprite(player_x,player_y,3)
         else:
             draw_sprite(player_x,player_y,5)
     else:
-        if move:
+        if move and player_frame%5 < 3:
             draw_sprite(player_x,player_y, 2)
         else:
             draw_sprite(player_x,player_y, 1)
@@ -240,6 +241,7 @@ while True:
         blt(worm._x, worm._y, 2, 0, 8, 16, 8, 3)
 
     worm_frame += 1
+    player_frame += 1
 
     flip()
     
